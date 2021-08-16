f8c3104ae6f8fe533a66acde3d39cd8e2dd44841
Vijay Tadimeti <tadimetv@oregonstate.edu>
2021-08-16 15:26:38 -0700

    validations to models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ActiveRecord::Base
+    validates :title, presence: true, length: {minimum: 10}
+    validates :body, presence: true, length: { minimum: 15}
+    validates :title, uniqueness: true
 end

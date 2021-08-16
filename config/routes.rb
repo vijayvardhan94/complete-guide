44c0147849efcae84f30f3c1b66f1927e5ae9c5b
Vijay Tadimeti <tadimetv@oregonstate.edu>
2021-08-16 14:35:30 -0700

    new create methods and views, and resourceful routing.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'articles#index'
-  get 'articles/index'
+  #get 'articles/index'
+  #get '/articles/:id', to: 'articles#show'
   
+  resources :articles
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
